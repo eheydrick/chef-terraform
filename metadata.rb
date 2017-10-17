9c978154ee16066182f972a41da4671e96eb0a9d
Eric Heydrick <eheydrick@gmail.com>
2017-10-17 16:56:58 -0700

    update ark dep to ~> 3.0

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ description      'Installs Terraform (terraform.io)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.2'
 
-depends 'ark', '~> 2.0'
+depends 'ark', '~> 3.0'
 depends 'build-essential', '~> 8.0'
 
 supports 'centos', '> 6.0'
